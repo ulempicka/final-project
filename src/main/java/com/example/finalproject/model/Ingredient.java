f7ead20df951d716f8ecf4d82fc0e907d3cea6cd
ulempicka <urszula.lempicka@gmail.com>
2020-09-22 19:52:55 +0200

    commit 2 started

---
+++ src/main/java/com/example/finalproject/model/Ingredient.java
@@ -0,0 +1,4 @@
+package com.example.finalproject.model;
+
+public class Ingredient {
+}
---
+++ src/main/java/com/example/finalproject/model/Recipe.java
@@ -0,0 +1,14 @@
+package com.example.finalproject.model;
+
+public class Recipe {
+
+    private Long id;
+    private String title;
+    private String description;
+    private int timePrep;
+    private String difficulty;  //enum
+    private String category;    //enum
+    private boolean like;
+    private double rating;
+
+}
